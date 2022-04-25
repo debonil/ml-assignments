247756b9865eb06e292397fbd8b23545580959b5
Debonil Ghosh <debonil.ghosh@gmail.com>
2022-04-26 00:33:43 +0530

    best model

---
+++ ProjectSem1/best-model.py
@@ -0,0 +1,149 @@
+
+import seaborn as sns
+from sklearn import metrics
+import numpy as np
+import torch
+import torchvision
+import matplotlib.pyplot as plt
+from time import time
+from torchvision import datasets, transforms
+from torch import nn, optim
+
+
+transform = transforms.Compose([transforms.Grayscale(), transforms.ToTensor(),
+                                transforms.Normalize((0.5,), (0.5,)),
+                                ])
+
+
+trainset = datasets.ImageFolder('cifar/train', transform=transform)
+valset = datasets.ImageFolder('cifar/test', transform=transform)
+trainloader = torch.utils.data.DataLoader(
+    trainset, batch_size=64, shuffle=True)
+valloader = torch.utils.data.DataLoader(valset, batch_size=64, shuffle=True)
+
+
+
+input_size = 3072
+hidden_sizes = [2048, 1024, 256]
+output_size = len(trainset.classes)
+
+model = nn.Sequential(nn.Linear(input_size, hidden_sizes[0]),
+                            nn.ReLU(),
+                            nn.Linear(hidden_sizes[0], hidden_sizes[1]),
+                            nn.ReLU(),
+                            nn.Linear(hidden_sizes[1], hidden_sizes[2]),
+                            nn.ReLU(),
+                            nn.Linear(hidden_sizes[2], output_size),
+                            nn.LogSoftmax(dim=1))
+
+
+def model_accuracy(input_size=1024, hidden_sizes=[512, 64], output_size=10):
+
+    model = nn.Sequential(nn.Linear(input_size, hidden_sizes[0]),
+                          nn.ReLU(),
+                          nn.Linear(hidden_sizes[0], hidden_sizes[1]),
+                          nn.ReLU(),
+                          nn.Linear(hidden_sizes[1], output_size),
+                          nn.LogSoftmax(dim=1))
+
+    images, labels = next(iter(trainloader))
+    images = images.view(images.shape[0], -1)
+
+    logps = model(images)  # log probabilities
+
+    criterion = nn.NLLLoss()
+
+    #tlabls = labelTransform(labels,10)
+    loss = criterion(logps, labels)  # calculate the loss
+
+    loss.backward()
+
+    optimizer = optim.SGD(model.parameters(), lr=0.003, momentum=0.9)
+    time0 = time()
+    epochs = 15
+    for e in range(epochs):
+        running_loss = 0
+        cnt = 0
+        for images, labels in trainloader:
+            # Flatten MNIST images into a 784 long vector
+            images = images.view(images.shape[0], -1)
+            cnt += images.shape[0]
+            # Training pass
+            optimizer.zero_grad()
+
+            output = model(images)
+            loss = criterion(output, labels)
+
+            # This is where the model learns by backpropagating
+            loss.backward()
+
+            # And optimizes its weights here
+            optimizer.step()
+
+            running_loss += loss.item()
+
+    correct_count, all_count = 0, 0
+    pred_label_arr = []
+    true_label_arr = []
+    for images, labels in valloader:
+        for i in range(len(labels)):
+            img = images[i].view(1, 1024)
+            # Turn off gradients to speed up this part
+            with torch.no_grad():
+                logps = model(img)
+
+            # Output of the network are log-probabilities, need to take exponential for probabilities
+            ps = torch.exp(logps)
+            probab = list(ps.numpy()[0])
+            pred_label = probab.index(max(probab))
+            true_label = labels.numpy()[i]
+            if(true_label == pred_label):
+                correct_count += 1
+            all_count += 1
+            pred_label_arr.append(pred_label)
+            true_label_arr.append(true_label)
+    #print("Number Of Images Tested =", all_count)
+    accuracy = (correct_count/all_count)
+    print(f"\n Hidden Layer = {hidden_sizes} Model Accuracy = {accuracy}")
+    return accuracy
+
+
+sns.set(rc={'axes.facecolor': 'lightblue', 'figure.facecolor': 'lightblue'})
+
+
+def confusionMatrixAndAccuracyReport(Y_test, Y_pred):
+    cm = metrics.confusion_matrix(Y_test, Y_pred)
+    overallAccuracy = np.trace(cm)/sum(cm.flatten())
+
+    classwiseAccuracy = np.zeros(len(cm))
+    for n in range(len(cm)):
+        for i in range(len(cm)):
+            for j in range(len(cm)):
+                if (i != n and j != n) or (i == n and j == n):
+                    classwiseAccuracy[n] += cm[i][j]
+
+    classwiseAccuracy /= sum(cm.flatten())
+
+    plt.figure(figsize=(6, 6))
+    plt.title('Accuracy Score: {0:3.3f}'.format(overallAccuracy), size=12)
+    plt.ylabel('Actual label')
+    plt.xlabel('Predicted label')
+    sns.heatmap(data=cm, annot=True, square=True,  cmap='Blues')
+
+    plt.show()
+    print('Overall Accuracy Score: {0:3.3f}'.format(overallAccuracy))
+    print('Classwise Accuracy Score: {0}'.format(classwiseAccuracy))
+
+
+accs = []
+accsLbl = []
+n=2
+for i in range(n):
+    for j in range(n):
+        for k in range(n):
+            accs.append(model_accuracy(hidden_sizes=[2**(11-i), 2**(11-j), 2**(11-k)]))
+            accsLbl.append([2**(11-i), 2**(11-j), 2**(11-k)])
+
+ij = 0 #np.argmax(np.array(accs))
+
+print(f'\n\nBest model :: Hidden Layer = {accsLbl} Model Accuracy = {accs[ij]}')
